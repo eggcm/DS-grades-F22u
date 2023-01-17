2c5f3d7a19d16ebeb64f4ded6890de8996bafb25
eggcm <52985883+eggcm@users.noreply.github.com>
2023-01-17 20:07:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ Papa.parse("./DS-grades-F22u.csv", {
 })
 
 function display() {
-    const id = document.getElementById('student-id').value;
+    const id = document.getElementById('sid-mid').value;
     if (id=="") return;
     // p.innerText = JSON.stringify(data[id]);
     const display = `
@@ -39,10 +39,8 @@ function display() {
     Fin:  ${data[id].Fin}
     CPE:  ${data[id].CPE}
     ATT:  ${data[id].ATT}
-    **
     Sem:  ${data[id].Sem}
-    **
-    The grade is determined by the following rule: (H1+H2+H3+H4+H5)/5*0.4+SQRT(Mid)*10*0.25+SQRT(FIN)*10*0.35+CPE</p>".
+    The grade is determined by the following rule: (H1+H2+H3+H4+H5)/5*0.4+SQRT(Mid)*10*0.25+SQRT(FIN)*10*0.35+CPE.
     NOTE thatthe highest grade of this course is 99. If your final grade is large than 99, then you will get 99 at most.
     `;
     p.innerText = display;
